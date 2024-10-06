fdf8aac33e20a3c0d87b2df7fd823cff3f680f16
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-06 23:20:36 +0300

    feat: add prisma

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,40 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+
+const prisma = new PrismaClient();
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
-server.get("/", (req, res) => {
-  res.json({
-    message: "Hello world",
+server.get("/course", async (req, res) => {
+  res.json(await prisma.course.findMany());
+});
+
+server.post("/course", async (req, res) => {
+  const body = req.body;
+
+  const course = await prisma.course.create({
+    data: {
+      name: body.name,
+    },
   });
+
+  res.json({
+    course,
+  });
+});
+
+server.delete("/course/:id", async (req, res) => {
+  await prisma.course.delete({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+
+  res.status(204).end();
 });
 
 server.listen(3000, () => {
